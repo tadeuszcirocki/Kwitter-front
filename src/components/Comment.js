d7f86638d62b2af7a8ac80743452733e9abc08e9
Tadeusz Cirocki <tadeusz.cirocki1@gmail.com>
2021-01-20 14:15:30 +0100

    complete readonly display with no styling

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,16 +2,42 @@ import React from 'react';
 
 class Blog extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            author : {"username" : "default"}
+        };
+    }
+
+    componentDidMount() {
+        this.GetAuthor();
+    }
+
+    GetAuthor() {
+        const postId = this.props.comment.id
+        fetch("https://localhost:44386/api/comments/"+postId+"/user")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        author: result
+                    });
+                }
+            )
+    }
+
 
 
 
 
     render() {
         const comment = this.props.comment
+        const author = this.state.author
         return (
             <div>
                 this is comment
                 {comment.content}
+                AUTHOR: {author.username}
                 delete edit
             </div>
         )
--- src/components/Post.js
+++ src/components/Post.js
@@ -3,16 +3,42 @@ import CommentSection from "./CommentSection";
 
 class Post extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            author : {"username" : "default"}
+        };
+    }
+
+    componentDidMount() {
+        this.GetAuthor();
+    }
+
+    GetAuthor() {
+        const postId = this.props.post.id
+        fetch("https://localhost:44386/api/kweets/"+postId+"/user")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        author: result
+                    });
+                }
+            )
+    }
+
 
 
 
 
     render() {
         const post = this.props.post;
+        const author = this.state.author
         return (
             <div>
                 this is post
                 TITLE: {post.title}
+                AUTHOR: {author.username}
                 <CommentSection postId={post.id}/>
             </div>
         )
